7378ab3b5fe72bd95337ad7b1756b9083b3188a8
nehaluddin.haider.mohammed <nehaluddin.haider.mohammed@intl.verizon.com>
2017-11-20 17:44:54 +0530

    Initial commit

---
+++ app/src/main/java/mobi/bluerocket/empmanager/listItems/BorrowedListViewModel.java
@@ -0,0 +1,53 @@
+package mobi.bluerocket.empmanager.listItems;
+
+import android.app.Application;
+import android.arch.lifecycle.AndroidViewModel;
+import android.arch.lifecycle.LiveData;
+import android.os.AsyncTask;
+
+import java.util.List;
+
+import mobi.bluerocket.empmanager.db.AppDatabase;
+import mobi.bluerocket.empmanager.db.BorrowModel;
+
+
+public class BorrowedListViewModel extends AndroidViewModel {
+
+    private final LiveData<List<BorrowModel>> itemAndPersonList;
+
+    private AppDatabase appDatabase;
+
+    public BorrowedListViewModel(Application application) {
+        super(application);
+
+        appDatabase = AppDatabase.getDatabase(this.getApplication());
+
+        itemAndPersonList = appDatabase.itemAndPersonModel().getAllBorrowedItems();
+    }
+
+
+    public LiveData<List<BorrowModel>> getItemAndPersonList() {
+        return itemAndPersonList;
+    }
+
+    public void deleteItem(BorrowModel borrowModel) {
+        new deleteAsyncTask(appDatabase).execute(borrowModel);
+    }
+
+    private static class deleteAsyncTask extends AsyncTask<BorrowModel, Void, Void> {
+
+        private AppDatabase db;
+
+        deleteAsyncTask(AppDatabase appDatabase) {
+            db = appDatabase;
+        }
+
+        @Override
+        protected Void doInBackground(final BorrowModel... params) {
+            db.itemAndPersonModel().deleteBorrow(params[0]);
+            return null;
+        }
+
+    }
+
+}
---
+++ app/src/main/java/mobi/bluerocket/empmanager/listItems/MainActivity.java
@@ -0,0 +1,65 @@
+package mobi.bluerocket.empmanager.listItems;
+
+import android.arch.lifecycle.Observer;
+import android.arch.lifecycle.ViewModelProviders;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import mobi.bluerocket.empmanager.R;
+import mobi.bluerocket.empmanager.addItem.AddActivity;
+import mobi.bluerocket.empmanager.db.BorrowModel;
+
+public class MainActivity extends AppCompatActivity implements View.OnLongClickListener {
+
+    private BorrowedListViewModel viewModel;
+    private RecyclerViewAdapter recyclerViewAdapter;
+    private RecyclerView recyclerView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, AddActivity.class));
+            }
+        });
+
+
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerViewAdapter = new RecyclerViewAdapter(new ArrayList<BorrowModel>(), this);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        recyclerView.setAdapter(recyclerViewAdapter);
+
+        viewModel = ViewModelProviders.of(this).get(BorrowedListViewModel.class);
+
+        viewModel.getItemAndPersonList().observe(MainActivity.this, new Observer<List<BorrowModel>>() {
+            @Override
+            public void onChanged(@Nullable List<BorrowModel> itemAndPeople) {
+                recyclerViewAdapter.addItems(itemAndPeople);
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onLongClick(View v) {
+        BorrowModel borrowModel = (BorrowModel) v.getTag();
+        viewModel.deleteItem(borrowModel);
+        return true;
+    }
+}
